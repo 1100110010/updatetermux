bbf835d216fba4a42b1f22406949062c206133da
1100110010 <75100630+1100110010@users.noreply.github.com>
2021-04-11 00:02:02 +0300

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,19 @@
+pkg uprage
+pkg update
+pkg upgrade
+apt uprage
+apt update
+apt upgrade
+apt-get uprage
+apt-get update
+apt-get upgrade
+pkg install python
+pkg install python2
+pkg install python3
+python -m pip install --upgrade pip
+pkg install git
+cd ..
+git clone https://github.com/ranginang67/DarkFly-tool
+cd DarkFly-tool
+chmod +x install.py
+python2 install.py
